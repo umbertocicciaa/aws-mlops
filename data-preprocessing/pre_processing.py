6293316f7fdaeee49f8e5cb3693979e5312f4aa3
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2025-05-14 19:52:27 +0200

    add pre processing script

---
+++ data-preprocessing/pre_processing.py
@@ -0,0 +1,37 @@
+from awsglue.context import GlueContext
+from awsglue.dynamicframe import DynamicFrame
+from pyspark.context import SparkContext
+
+glueContext = GlueContext(SparkContext.getOrCreate())
+
+# Glue Data Catalog: database and table name
+db_name = "housings"
+tbl_name = "housings_table"
+
+# S3 location for output
+output_dir = "s3://llUY-pre-processed-data-bucket/housing_parquet"
+
+# Read data into a DynamicFrame using the Data Catalog metadata
+housing_dyf = glueContext.create_dynamic_frame.from_catalog(database=db_name, table_name=tbl_name)
+
+# Remove records with missing values
+housing_df = housing_dyf.toDF()
+housing_df = housing_df.dropna()
+
+# Turn it back to a dynamic frame
+housing_tmp = DynamicFrame.fromDF(housing_df, glueContext, "nested")
+
+# Rename, cast, and nest with apply_mapping
+medicare_nest = housing_tmp.apply_mapping([('longitude', 'double', 'location.longitude', 'double'),
+                                            ('latitude', 'double', 'location.latitude', 'double'),
+                                            ('housing_median_age', 'double', 'housing.age', 'double'),
+                                            ('total_rooms', 'double', 'housing.total_rooms', 'double'),
+                                            ('total_bedrooms', 'double', 'housing.total_bedrooms', 'double'),
+                                            ('population', 'double', 'housing.population', 'double'),
+                                            ('households', 'double', 'housing.households', 'double'),
+                                            ('median_income', 'double', 'income.median', 'double'),
+                                            ('median_house_value', 'double', 'price.median', 'double'),
+                                            ('ocean_proximity', 'string', 'location.ocean_proximity', 'string')])
+
+# Write it out in Parquet
+glueContext.write_dynamic_frame.from_options(frame=medicare_nest, connection_type="s3", connection_options={"path": output_dir}, format="parquet")
