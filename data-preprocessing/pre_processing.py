37d94beef54eb3fa34050dcbc71b8ea040316ab6
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2025-05-22 09:18:22 +0200

    fix glue preprocessing resolver

--- data-preprocessing/pre_processing.py
+++ data-preprocessing/pre_processing.py
@@ -1,4 +1,5 @@
-import os
+import sys
+from awsglue.utils import getResolvedOptions
 from awsglue.context import GlueContext
 from awsglue.dynamicframe import DynamicFrame
 from pyspark.context import SparkContext
@@ -7,10 +8,13 @@ glueContext = GlueContext(SparkContext.getOrCreate())
 
 # Glue Data Catalog: database and table name
 db_name = "housings"
-tbl_name = "housings_table"
+tbl_name = "input"
 
 # S3 location for output
-output_dir = os.environ.get("OUTPUT_DIR")
+args = getResolvedOptions(sys.argv,
+                          ['JOB_NAME',
+                           'OUTPUT_DIR',])
+output_dir = args['OUTPUT_DIR']
 
 # Read data into a DynamicFrame using the Data Catalog metadata
 housing_dyf = glueContext.create_dynamic_frame.from_catalog(database=db_name, table_name=tbl_name)
