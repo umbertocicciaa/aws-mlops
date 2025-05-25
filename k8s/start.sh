b2951024a0c31d01123c2a042cdcd818a7c78ecc
Umberto Domenico Ciccia <108148943+umbertocicciaa@users.noreply.github.com>
2025-05-25 12:46:04 +0200

    improve documentation of star.sh script

--- k8s/start.sh
+++ k8s/start.sh
@@ -1,3 +1,4 @@
+# Docker deamon
 echo "Starting Docker daemon..."
 open --background -a Docker
 
@@ -6,22 +7,22 @@ while ! docker system info > /dev/null 2>&1; do
     sleep 2
 done
 
-
+# Minikube setup
 echo "Starting minikube..."
 minikube start
-
 kubectl config use-context minikube
 
 echo "Applying Kubernetes manifests..."
-
 while ! minikube status | grep -q "host: Running"; do
     echo "Waiting for minikube to be ready..."
     sleep 2
 done
 
+# Kubernetes manifests
 kubectl apply -f .
 echo "Deployment complete."
 
+# Open minikube tunnel for service access
 echo "Opening minikube tunnel for service access..."
 minikube tunnel &
 echo "Minikube tunnel started in the background."
