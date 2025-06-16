e484b2515c3cb54031dff8815d63f06733627543
Umberto Domenico Ciccia <umbertociccia@icloud.com>
2025-06-16 22:55:26 +0200

    Update clean-aws.sh

--- clean-aws.sh
+++ clean-aws.sh
@@ -1,6 +1,11 @@
-# Scripts to clean up AWS Model's resources created by the sagemaker.
-# You cant delete AWS Model's resources with terraform destroy because are not part ot the infrastructure.
+#!/bin/bash
+
+# Scripts to clean up AWS Model's resources created by SageMaker.
+# Terraform can't destroy these because they are managed outside the IaC scope.
+
 MODEL_PACKAGE_GROUP_NAME="<your-model-package-group-name>"
+ENDPOINT_NAME="<your-endpoint-name>"
+ENDPOINT_CONFIG_NAME="<your-endpoint-config-name>"
 
 for arn in $(aws sagemaker list-model-packages \
     --model-package-group-name "$MODEL_PACKAGE_GROUP_NAME" \
@@ -10,5 +15,18 @@ for arn in $(aws sagemaker list-model-packages \
   aws sagemaker delete-model-package --model-package-name "$arn"
 done
 
+echo "Deleting model package group: $MODEL_PACKAGE_GROUP_NAME"
 aws sagemaker delete-model-package-group \
   --model-package-group-name "$MODEL_PACKAGE_GROUP_NAME"
+
+echo "Deleting endpoint: $ENDPOINT_NAME"
+aws sagemaker delete-endpoint \
+  --endpoint-name "$ENDPOINT_NAME"
+
+echo "Waiting for endpoint deletion to complete..."
+aws sagemaker wait endpoint-deleted \
+  --endpoint-name "$ENDPOINT_NAME"
+
+echo "Deleting endpoint configuration: $ENDPOINT_CONFIG_NAME"
+aws sagemaker delete-endpoint-config \
+  --endpoint-config-name "$ENDPOINT_CONFIG_NAME"
